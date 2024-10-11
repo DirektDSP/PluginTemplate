79f1a90935b57a79ca9ffb6a8a36cf796f20c538
Seamus Mullan <43112447+SeamusMullan@users.noreply.github.com>
2024-10-11 10:27:17 +0100

    EULA edit + Actions rename

---
+++ generator/main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+# Function of this script:
+# Take in a new project name and any other into (plugin manufacturer, code, etc)
+# Replace any relevant variables in other files with the new info so new projects can be created quicker
+
